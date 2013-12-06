aa80f7b0215e44b6f05895d2310121a4faae3561
yuelenghan <lh_1986@foxmail.com>
2013-12-06 09:56:41 +0800

    完成通讯录的删除功能 增加增删改的等待进度条 完善增删改中出现的一些问题

--- newsPaper/controller/ContactsController.js
+++ newsPaper/controller/ContactsController.js
@@ -49,6 +49,9 @@ Ext.define('NewsPaper.controller.ContactsController', {
         var tree = Ext.getCmp('contactsTypeTreeView');
         var node = tree.getSelectionModel().getSelection()[0];
         if (node) {
+            var progress = Ext.MessageBox.wait('正在添加通讯录类别', '添加', {
+                text: '添加中...'
+            });
             var store = tree.store;
             Ext.Ajax.request({
                 url: '/newsPaper/contactsType/addChild',
@@ -58,6 +61,7 @@ Ext.define('NewsPaper.controller.ContactsController', {
                     text: '新添加'
                 },
                 success: function (response, opts) {
+                    progress.close();
                     var result = response.responseText;
                     if (result.toUpperCase() == "SUCCESS") {
                         Ext.example.msg('添加成功', '添加通讯录类别成功!');
@@ -69,6 +73,7 @@ Ext.define('NewsPaper.controller.ContactsController', {
 
                 },
                 failure: function (response, opts) {
+                    progress.close();
                     Ext.example.msg('添加失败', '添加通讯录类别失败!');
                     treeRefresh(store, tree);
                 }
@@ -88,7 +93,12 @@ Ext.define('NewsPaper.controller.ContactsController', {
                         Ext.MessageBox.alert('错误', '不能删除根节点!');
                         return;
                     }
+                    var progress = Ext.MessageBox.wait('正在删除通讯录类别', '删除', {
+                        text: '删除中...'
+                    });
                     var store = tree.store;
+                    var gridStore = Ext.getCmp('contactsGridView').getStore();
+
                     Ext.Ajax.request({
                         url: '/newsPaper/contactsType/removeContactsType',
                         method: 'post',
@@ -96,6 +106,7 @@ Ext.define('NewsPaper.controller.ContactsController', {
                             id: node.data.id
                         },
                         success: function (response, opts) {
+                            progress.close();
                             var result = response.responseText;
                             if (result.toUpperCase() == "SUCCESS") {
                                 Ext.example.msg('删除成功', '删除通讯录类别成功!');
@@ -103,12 +114,14 @@ Ext.define('NewsPaper.controller.ContactsController', {
                                 Ext.example.msg('删除失败', '删除通讯录类别失败!');
                             }
 
+                            // 删除后刷新contactsType树,选中根节点,加载contacts
                             treeRefresh(store, tree);
-
+                            tree.getSelectionModel().select(tree.getRootNode());
+                            gridStore.loadPage(1);
                         },
                         failure: function (response, opts) {
+                            progress.close();
                             Ext.example.msg('删除失败', '删除通讯录类别失败!');
-                            treeRefresh(store, tree);
                         }
                     });
                 }
@@ -125,6 +138,9 @@ Ext.define('NewsPaper.controller.ContactsController', {
         if (newValue != oldValue) {
             var id = e.record.data.id;
             //alert('newValue = ' + newValue + ", id = " + id);
+            var progress = Ext.MessageBox.wait('正在编辑通讯录类别', '编辑', {
+                text: '编辑中...'
+            });
             Ext.Ajax.request({
                 url: '/newsPaper/contactsType/updateContactsType',
                 method: 'post',
@@ -133,6 +149,7 @@ Ext.define('NewsPaper.controller.ContactsController', {
                     name: newValue
                 },
                 success: function (response, opts) {
+                    progress.close();
                     var result = response.responseText;
                     if (result.toUpperCase() == "SUCCESS") {
                         Ext.example.msg('编辑成功', '编辑通讯录类别成功!');
@@ -142,6 +159,7 @@ Ext.define('NewsPaper.controller.ContactsController', {
                     treeRefresh(store, tree);
                 },
                 failure: function (response, opts) {
+                    progress.close();
                     Ext.example.msg('编辑失败', '编辑通讯录类别失败!');
                     treeRefresh(store, tree);
                 }
@@ -197,6 +215,42 @@ Ext.define('NewsPaper.controller.ContactsController', {
     },
     removeContactsClick: function () {
         //alert("remove");
+        var grid = Ext.getCmp('contactsGridView');
+        var record = grid.getSelectionModel().getSelection()[0];
+        if (record) {
+            Ext.MessageBox.confirm('确认删除', '确定删除此通讯录人员?', function (btn) {
+                if (btn == 'yes') {
+                    var store = grid.getStore();
+                    var progress = Ext.MessageBox.wait('正在删除通讯录', '删除', {
+                        text: '删除中...'
+                    });
+                    Ext.Ajax.request({
+                        url: '/newsPaper/contacts/removeContacts',
+                        method: 'post',
+                        params: {
+                            id: record.get('id')  // 或者record.data.id
+                        },
+                        success: function (response) {
+                            progress.close();
+                            var result = response.responseText;
+                            if (result.toUpperCase() == "SUCCESS") {
+                                Ext.example.msg('删除成功', '删除通讯录成功!');
+                            } else {
+                                Ext.example.msg('删除失败', '删除通讯录失败!');
+                            }
+                            store.reload();
+                        },
+                        failure: function () {
+                            progress.close();
+                            Ext.example.msg('删除失败', '删除通讯录失败!');
+                        }
+                    });
+                }
+            });
+        } else {
+            Ext.MessageBox.alert('错误', '请选择一条记录！');
+        }
+
     },
     editContacts: function (editor, context, eOpts) {
         var oldValues = context.originalValues;
@@ -205,6 +259,10 @@ Ext.define('NewsPaper.controller.ContactsController', {
             || oldValues.idCard != newValues.idCard
             || oldValues.phone != newValues.phone
             || oldValues.email != newValues.email) {
+
+            var progress = Ext.MessageBox.wait('正在编辑通讯录', '编辑', {
+                text: '编辑中...'
+            });
 
             var grid = Ext.getCmp('contactsGridView');
             var store = grid.getStore();
@@ -219,6 +277,7 @@ Ext.define('NewsPaper.controller.ContactsController', {
                     email: newValues.email
                 },
                 success: function (response) {
+                    progress.close();
                     var result = response.responseText;
                     if (result.toUpperCase() == "SUCCESS") {
                         Ext.example.msg('编辑成功', '编辑通讯录成功!');
@@ -228,6 +287,7 @@ Ext.define('NewsPaper.controller.ContactsController', {
                     store.reload();
                 },
                 failure: function (response) {
+                    progress.close();
                     Ext.example.msg('编辑失败', '编辑通讯录失败!');
                 }
             });
@@ -245,18 +305,23 @@ Ext.define('NewsPaper.controller.ContactsController', {
         var store = grid.getStore();
         var form = Ext.getCmp('contactsForm').getForm();
         if (form.isValid()) {
+            var progress = Ext.MessageBox.wait('正在添加通讯录', '添加', {
+                text: '添加中...'
+            });
             form.submit({
                 params: {
                     'contactsType.id': id
                 },
                 success: function (form, action) {
                     //Ext.Msg.alert('Success', action.result.msg);
+                    progress.close();
                     Ext.example.msg('增加成功', '增加通讯录人员成功!');
                     window.close();
                     store.reload();
                 },
                 failure: function (form, action) {
                     //Ext.Msg.alert('Failed', action.result.msg);
+                    progress.close();
                     Ext.example.msg('增加失败', '增加通讯录人员失败!');
                     window.close();
                     store.reload();
