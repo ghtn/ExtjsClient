1860d701ffd5f72b25161eadab5e95fcb56f3b56
yuelenghan <lh_ztt@qq.com>
2014-07-11 11:05:34 +0800

    完成提交试卷的功能.

--- js/exam.js
+++ js/exam.js
@@ -2,11 +2,14 @@
  * Created by lihe on 14-7-8.
  */
 var loading = false;
+var subjectList;
+
+// 从sessionStorage中获取登录人员的身份证号和考试id
+var idCardInfo = sessionStorage.getItem("idCardInfo");
+var examId = sessionStorage.getItem("examId");
 
 function loadPaper() {
     if (loading == false) {
-        var examId = sessionStorage.getItem("examId");
-
         $.mobile.loading("show", {text: "正在获取试卷信息...", textVisible: true});
         loading = true;
 
@@ -20,7 +23,7 @@ function loadPaper() {
                 if (data != undefined && data != null && data.code == 1) {
                     var paperDiv = $("#paperDiv");
                     paperDiv.html("");
-                    var subjectList = data.subjectList;
+                    subjectList = data.subjectList;
                     var divStr = "";
                     for (var i = 0; i < subjectList.length; i++) {
                         var subject = subjectList[i];
@@ -97,10 +100,6 @@ function loadPaper() {
 }
 
 function chooseAnswer(subjectId, answerId, subjectType, checkboxName) {
-    // 从sessionStorage中获取登录人员的身份证号和考试id
-    var idCardInfo = sessionStorage.getItem("idCardInfo");
-    var examId = sessionStorage.getItem("examId");
-
     if (subjectType == 0) {
         // 单选题
         // 存储答题信息, "考试id#身份证号#题目id" 作为key, 答案id作为value
@@ -119,17 +118,12 @@ function chooseAnswer(subjectId, answerId, subjectType, checkboxName) {
             answerStr += $(this).val() + "#";
         });
         answerStr = answerStr.substr(0, answerStr.length - 1); // 去掉字符串中的最后一个"#"
-//        alert(answerStr);
         localStorage.setItem(examId + "#" + idCardInfo + "#" + subjectId, answerStr);
     }
 
 }
 
 function loadAnswer(subject) {
-    // 从sessionStorage中获取登录人员的身份证号和考试id
-    var idCardInfo = sessionStorage.getItem("idCardInfo");
-    var examId = sessionStorage.getItem("examId");
-
     var keyStr = examId + "#" + idCardInfo + "#" + subject.id;
     var answerStr = localStorage.getItem(keyStr);
 
@@ -159,4 +153,57 @@ function loadAnswer(subject) {
         }
     }
 
+}
+
+function finishExam() {
+    if (loading == false) {
+        var paramStr = "";
+        for (var i = 0; i < subjectList.length; i++) {
+            var keyStr = examId + "#" + idCardInfo + "#" + subjectList[i].id;
+            var answerStr = localStorage.getItem(keyStr);
+            paramStr += keyStr + "#" + answerStr + "@";
+        }
+
+        paramStr = paramStr.substr(0, paramStr.length - 1); //去掉最后一个"@"
+
+        $.mobile.loading("show", {text: "正在提交答题信息...", textVisible: true});
+        loading = true;
+
+        $.ajax({
+            url: "/InformationSystemService/exam/finishExam",
+            data: "paramStr=" + paramStr,
+            dataType: "json",
+            type: "post",
+            timeout: 10000,
+            success: function (data) {
+                if (data != undefined && data != null && data.code == 1) {
+                    $().toastmessage('showToast', {
+                        text: '提交答题信息成功！考试成绩为 : ' + data.examScore + "分, 一共错了" + data.errorCount + "题!",
+                        sticky: true,
+                        position: 'middle-center',
+                        type: 'success'
+                    });
+                } else {
+                    $().toastmessage('showToast', {
+                        text: '提交答题信息失败！' + data.msg,
+                        sticky: true,
+                        position: 'middle-center',
+                        type: 'error'
+                    });
+                }
+                $.mobile.loading("hide");
+                loading = false;
+            },
+            error: function () {
+                $().toastmessage('showToast', {
+                    text: '访问服务器错误！',
+                    sticky: true,
+                    position: 'middle-center',
+                    type: 'error'
+                });
+                $.mobile.loading("hide");
+                loading = false;
+            }
+        });
+    }
 }
